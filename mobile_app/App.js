dd710fb1f770bb3289c6b547171e568c919e43e5
Cedric <ledenmat.cedric@gmail.com>
2022-03-05 22:17:05 -0500

    Ejected Expo
    Fixed build issues and created project structure

--- mobile_app/App.js
+++ mobile_app/App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Camera } from 'expo-camera';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen name="Image" component={ImageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+const ImageScreen = ({navigation, route}) => {
+  return <Image style= {{ height:"100%", width: "100%" }} source={{ uri: route.params.uri}} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  }
 });
+
+const CameraScreen = ({navigation}) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const camera = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  snap = async () => {
+    if (camera.current) {
+      let photo = await camera.current.takePictureAsync({});
+      navigation.navigate('Image', {uri: photo.uri})
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ratio="16:9" ref={camera}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={snap}>
+            <Text style={styles.text}> Snap </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+
+}
